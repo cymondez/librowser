16328db58953739a7e646c5cf6522dfea764ab30
rrooyycn <rrooyycn@0bff3f9e-0306-11df-bca2-d32778fd5087>
2010-01-20 13:28:45 +0000

    初始版本

---
+++ src/Bootstrap/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Bootstrap
+{
+   partial class MainForm
+   {
+      /// <summary>
+      /// 必需的设计器变量。
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// 清理所有正在使用的资源。
+      /// </summary>
+      /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+      protected override void Dispose( bool disposing )
+      {
+         if ( disposing && ( components != null ) )
+         {
+            components.Dispose();
+         }
+         base.Dispose( disposing );
+      }
+
+      #region Windows 窗体设计器生成的代码
+
+      /// <summary>
+      /// 设计器支持所需的方法 - 不要
+      /// 使用代码编辑器修改此方法的内容。
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.browser = new Skybound.Gecko.GeckoWebBrowser();
+         this.SuspendLayout();
+         // 
+         // browser
+         // 
+         this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.browser.Location = new System.Drawing.Point( 0, 0 );
+         this.browser.Name = "browser";
+         this.browser.Size = new System.Drawing.Size( 732, 498 );
+         this.browser.TabIndex = 0;
+         // 
+         // MainForm
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF( 8F, 15F );
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size( 732, 498 );
+         this.Controls.Add( this.browser );
+         this.Name = "MainForm";
+         this.Text = "观辰专用浏览器";
+         this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+         this.Load += new System.EventHandler( this.Form1_Load );
+         this.ResumeLayout( false );
+
+      }
+
+      #endregion
+
+      private Skybound.Gecko.GeckoWebBrowser browser;
+   }
+}
+
---
+++ src/Bootstrap/MainForm.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Skybound.Gecko;
+
+namespace Bootstrap
+{
+   public partial class MainForm : Form
+   {
+      public MainForm()
+      {
+         InitializeComponent();
+      }
+
+      private void Form1_Load( object sender, EventArgs e )
+      {
+         browser.Navigate( "http://www.unigc.com/" );
+         //browser.Navigate( "http://a.unigc.com/jppw" );
+         browser.CreateWindow += new GeckoCreateWindowEventHandler( browser_CreateWindow );
+         //browser.
+         //Skybound.Gecko.GeckoPreferences.Save( @"c:\1.txt" );
+         browser.Navigated += new GeckoNavigatedEventHandler( browser_Navigated );
+         browser.DocumentCompleted += new EventHandler( browser_DocumentCompleted );
+      }
+
+      void browser_DocumentCompleted( object sender, EventArgs e )
+      {
+         GeckoWebBrowser gwb = (GeckoWebBrowser)sender;
+         gwb.SaveDocument( @"c:\1.txt" );
+      }
+
+      private void browser_Navigated( object sender, GeckoNavigatedEventArgs e )
+      {
+         //GeckoWebBrowser gwb = (GeckoWebBrowser) sender;
+         //MessageBox.Show( gwb.DocumentTitle );
+         //gwb.SaveDocument( @"c:\1.txt" );
+      }
+
+      private static void browser_CreateWindow( object sender, GeckoCreateWindowEventArgs e )
+      {
+         //MessageBox.Show( browser.StatusText );
+         //PopForm pop = new PopForm( browser.StatusText );
+         //pop.ShowDialog( this );
+         //MessageBox e.Flags == browser.StatusText
+         //throw new Exception( "The method or operation is not implemented." );
+         ChromeDialog dialog = new ChromeDialog();
+         e.WebBrowser = dialog.WebBrowser;
+         dialog.WebBrowser.CreateWindow += new GeckoCreateWindowEventHandler( browser_CreateWindow );
+         dialog.WindowState = FormWindowState.Maximized;
+         dialog.FormBorderStyle = FormBorderStyle.Sizable;
+         dialog.MaximizeBox = true;
+         dialog.MinimizeBox = true;
+         dialog.Show();
+         //dialog.WebBrowser.Window.Document.
+      }
+   }
+}
